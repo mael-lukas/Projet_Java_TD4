c51a24d80147fae51e2557698ad3e69a7a51c7bb
mael-lukas <mael.lukas@ensea.fr>
2024-11-29 15:37:10 +0100

    added second map, switching between maps using 'doors' and knockback effect when walking on traps

--- src/PlaygroundManager.java
+++ src/PlaygroundManager.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class PlaygroundManager {
     public ArrayList<Playground> playgroundList;
     private int maxPlayground = 10;
-    public int currentPlayground = 0;
+    public int currentPlayground = 2;
     Camera camera;
 
     public PlaygroundManager(Camera camera) {
@@ -11,6 +11,7 @@ public class PlaygroundManager {
         this.playgroundList = new ArrayList<Playground>();
         playgroundList.add(0,new Playground("./data/large_map1.txt",66,56,camera));
         playgroundList.add(1,new Playground("./data/large_map2.txt",50,46,camera));
+        playgroundList.add(2,new Playground("./data/large_map3.txt",50,50,camera));
     }
 
     public void setCurrentPlayground(int currentPlayground) {
